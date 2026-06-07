ae626b8a7869acb24532a3265c6093cd9010763c
Atirti <139679060+Atirti@users.noreply.github.com>
2026-06-07 20:00:34 +0500

    add jwt service

--- dmc_back/dependencies.py
+++ dmc_back/dependencies.py
@@ -1,1 +1,19 @@
+import jwt
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends
+from services.jwt_service import JwtService
+import config
+import database
+
+Settings = config.Settings()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+jwt_service = JwtService(Settings.get_jwt_secret())
+
+def get_jwt_service():
+    return jwt_service
+
+
+def get_current_user(token: str = Depends(oauth2_scheme),
+                     jwt_service: JwtService = Depends(get_jwt_service)):
+    return jwt_service.decode(token)
 
---
+++ dmc_back/services/jwt_service.py
@@ -0,0 +1,57 @@
+import jwt
+import datetime
+import secrets
+from fastapi import HTTPException, status
+
+
+class JwtService:
+    def __init__(self, secrets_dct: dict):
+        self.secret = secrets_dct["secret"]
+        self.algorithm = secrets_dct["algorithm"]
+        self.exp_minutes = secrets_dct["exp_minutes"]
+        self.refresh_exp_days = secrets_dct["refresh_exp_days"]
+
+
+    def create_token_pair(self, user_id: int, username: str) -> dict[str, str]:
+        """
+        generate JWT and Refresh tokens
+        """
+        payload = {
+            "user_id": user_id,
+            "username": username,
+            "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=self.exp_minutes)
+        }
+
+        refresh_token = secrets.token_urlsafe(32)
+        # TODO put refresh token in database.
+
+        return {"jwt_token": jwt.encode(payload, self.secret, algorithm=self.algorithm),
+                "refresh_token": refresh_token}
+
+    def refresh_token(self, refresh_token: str) -> dict[str, str]:
+        """
+        finds user using refresh token in database and returns a new pair of jwt and refresh tokens
+        """
+        # TODO find user in db
+        user_id = 0
+        username = "delete later"
+
+        tokens_pair = self.create_token_pair(user_id, username)
+
+        #TODO update refresh token in db
+
+        return tokens_pair
+
+    def decode(self, token ) -> dict:
+        """
+        decodes jwt token and returns user id and username
+        """
+        try:
+            payload = jwt.decode(token, self.secret, algorithms=self.algorithm)
+            return {"user_id": payload["user_id"], "username": payload["username"]}
+
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired")
+        except jwt.InvalidTokenError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
