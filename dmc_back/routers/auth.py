ab958334b3587086850cd71fdb1e8fe8ceef15af
Atirti <139679060+Atirti@users.noreply.github.com>
2026-06-07 20:01:13 +0500

    add validators for auth

--- dmc_back/routers/auth.py
+++ dmc_back/routers/auth.py
@@ -1,31 +1,34 @@
 from fastapi import APIRouter
-from schemas.auth import UserModel, TokenModel
+from schemas.auth import LoginRequest, TokenRequest, LoginResponse, TokenResponse
 import dependencies
 
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
-@router.post("/login")
-async def login(user_model: UserModel):
+@router.post("/login", response_model=LoginResponse)
+async def login(user_model: LoginRequest) -> LoginResponse:
     """
-    login
-    :return: JSON with jwt and refresh tokens
-    """
-    pass
-
-@router.post("/regestration")
-async def registration(user_model: UserModel):
-    """
-    registration
-    :return: JSON with jwt and refresh tokens
+    returns jwt_token and refresh_token
     """
     pass
 
-@router.post("/refresh_token")
-async def refresh_token(token_model: TokenModel):
+@router.post("/regestration", response_model=LoginResponse)
+async def registration(user_model: LoginRequest) -> LoginResponse:
+    """
+    returns jwt_token and refresh_token
+    """
+    pass
+
+@router.post("/refresh_token", response_model=TokenResponse)
+async def refresh_token(token_model: TokenRequest) -> TokenResponse:
     """
     update jwt and refresh tokens
-    :return: JSON with jwt and refresh tokens
+    returns jwt_token and refresh_token
     """
+    pass
+
+@router.delete("/logout")
+async def logout() -> None:
+    """delete refresh token"""
     pass
