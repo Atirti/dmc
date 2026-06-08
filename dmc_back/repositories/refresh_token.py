409ee730d5db04021a28fd7ed5265909b8a4844e
Atirti <139679060+Atirti@users.noreply.github.com>
2026-06-09 02:15:04 +0500

    add init in services, add RefreshTokenRepository

---
+++ dmc_back/repositories/refresh_token.py
@@ -0,0 +1,16 @@
+from models import RefreshToken
+
+class RefreshTokenRepository:
+
+    def __init__(self, db):
+        self.db = db
+
+    def add_refresh_token():
+
+        pass
+
+    def remove_refresh_token():
+        pass
+
+    def update_refresh_token():
+        pass
