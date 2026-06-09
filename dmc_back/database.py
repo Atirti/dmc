17c128dc6c279fee8a041a6f8b0712a2bcd308af
Atirti <139679060+Atirti@users.noreply.github.com>
2026-06-09 18:09:10 +0500

    fix syntax for ssl

--- dmc_back/database.py
+++ dmc_back/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 class Database():
     def __init__(self, db_url):
-        self.engine = create_async_engine(url=db_url, connect_args={"sslmode": "require"})
+        self.engine = create_async_engine(url=db_url, connect_args={"ssl": "require"})
         self.session_factory = async_sessionmaker(bind=self.engine)
 
     async def get_sesion(self):
