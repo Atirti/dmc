05175dc6b6151d6d08dc9c64854a2d31790dd61c
Etrei1 <catinthenight89@gmail.com>
2026-06-10 02:30:20 +0500

    допил прошлого комита

--- dmc_front/src/JSX Windows/Autin/LoginWindow/LoginWIndow.jsx
+++ dmc_front/src/JSX Windows/Autin/LoginWindow/LoginWIndow.jsx
@@ -1,11 +1,26 @@
 import "./LoginWindowCSS.css";
 import {NavLink, useNavigate} from "react-router-dom";
+import {useState} from "react";
+import {loginRequest} from "../../../APIStuff/auth.js";
+
 
 export default function LoginWindow() {
     const navigate = useNavigate();
+    const [error, setError] = useState("");
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
 
-    function handleNavigate() {
-        navigate("/home");
+    async function handleLogin() {
+        try {
+            setError("");
+
+            const tokens = await loginRequest(login, password);
+            document.cookie = `jwt_token=${tokens.jwt_token}; path=/; SameSite=Lax`;
+            document.cookie = `refresh_token=${tokens.refresh_token}; path=/; SameSite=Lax`;
+            navigate("/home");
+        } catch (error) {
+            setError(error.message);
+        }
     }
 
     return (
@@ -19,14 +34,17 @@ export default function LoginWindow() {
                     <div className="Inputs">
                         <div className="Login">
                             <div className="inputLogin">
-                                <input placeholder=" " type="text" />
+                                <input placeholder=" " type="text" value={login}
+                                       onChange={(event) => setLogin(event.target.value)}/>
                                 <label>Логин</label>
                             </div>
                         </div>
 
                         <div className="Password">
                             <div className="inputPassword">
-                                <input placeholder=" " type="password" />
+                                <input placeholder=" " type="password"
+                                       value={password}
+                                       onChange={(event) => setPassword(event.target.value)}/>
                                 <label>Пароль</label>
                             </div>
                         </div>
@@ -38,9 +56,10 @@ export default function LoginWindow() {
                     </div>
 
                     <div className="ButtonContainer">
-                        <button className="loginButton" onClick={handleNavigate}>
+                        <button className="loginButton" onClick={handleLogin}>
                             Войти
                         </button>
+                        {error && <div className="errorMessage">{error}</div>}
                     </div>
                 </div>
             </div>
