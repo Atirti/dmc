a18238025fe64c2abea9f5ef2f6cd9e5b45efa25
Atirti <139679060+Atirti@users.noreply.github.com>
2026-06-09 21:54:14 +0500

    fix names

--- dmc_back/dependencies.py
+++ dmc_back/dependencies.py
@@ -13,17 +13,17 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 db = database.Database(Settings.get_db_async_url())
 
 
-async def __get_db():
+async def get_db():
     async for session in db.get_sesion():
         yield session
 
-def get_jwt_service(db: AsyncSession = Depends(__get_db)) -> JwtService:
+def get_jwt_service(db: AsyncSession = Depends(get_db)) -> JwtService:
     return JwtService(Settings.get_jwt_secret(), RefreshTokenRepository(db))
 
 def get_current_user(token: str = Depends(oauth2_scheme),
                      jwt_service: JwtService = Depends(get_jwt_service)) -> dict:
     return jwt_service.decode(token)
 
-def get_auth_service(db: AsyncSession = Depends(__get_db)) -> AuthService:
+def get_auth_service(db: AsyncSession = Depends(get_db)) -> AuthService:
     return AuthService(UserRepository(db))
 
