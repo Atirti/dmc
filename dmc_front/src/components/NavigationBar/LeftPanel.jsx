87e8f170a53c7a0d5a39372e859c0cbfcec0107f
Etrei1 <catinthenight89@gmail.com>
2026-06-10 02:30:20 +0500

    допил прошлого комита

--- dmc_front/src/components/NavigationBar/LeftPanel.jsx
+++ dmc_front/src/components/NavigationBar/LeftPanel.jsx
@@ -1,24 +1,42 @@
-import { MdAccountCircle, MdHome,MdShoppingCart,MdLogout } from "react-icons/md";
-import { NavLink } from "react-router-dom";
+import { MdAccountCircle, MdHome, MdShoppingCart, MdLogout } from "react-icons/md";
+import { NavLink, useNavigate } from "react-router-dom";
 import "./LeftPanelCSS.css";
+import {logoutRequest} from "../../APIStuff/auth.js";
+
 
 function LeftPanel() {
+    const navigate = useNavigate();
+
+    async function handleLogout() {
+        try {
+            await logoutRequest();
+            navigate("/");
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+
     return (
             <div className="LeftPanel">
                 <header className="shopTitle">DMC Store</header>
 
                 <div className="menu">
                     <NavLink to="/profile" className={({ isActive }) => isActive ? "active" : ""}>
-                        <MdAccountCircle size={25}/>Профиль</NavLink>
+                        <MdAccountCircle size={25} />
+                        Профиль</NavLink>
                     <NavLink to="/home" className={({ isActive }) => isActive ? "active" : ""}>
-                        <MdHome size={25}/>Главная</NavLink>
+                        <MdHome size={25} />
+                        Главная</NavLink>
+
                     <NavLink to="/cart" className={({ isActive }) => isActive ? "active" : ""}>
-                        <MdShoppingCart size={25}/>Корзина<i className="dot"></i></NavLink>
+                        <MdShoppingCart size={25} />
+                        Корзина
+                        <i className="dot"></i></NavLink>
                 </div>
-                <NavLink to="/" className="Exit">
-                    <MdLogout size={25}/>
-                    Выход
-                </NavLink>
+
+                <button onClick={handleLogout} className="Exit">
+                    <MdLogout size={25} />
+                    Выход</button>
             </div>
     );
 }
