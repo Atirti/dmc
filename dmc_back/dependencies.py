55a90d70f3102ea0b7516aac90390e792135cb53
Atirti <139679060+Atirti@users.noreply.github.com>
2026-06-09 18:08:55 +0500

    add auth service in dependencies.py, make fields private

--- dmc_back/dependencies.py
+++ dmc_back/dependencies.py
@@ -3,8 +3,8 @@ from fastapi.security import OAuth2PasswordBearer
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from services import JwtService
-from repositories import RefreshTokenRepository
+from services import JwtService, AuthService
+from repositories import RefreshTokenRepository, UserRepository
 import config
 import database
 
@@ -13,14 +13,17 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 db = database.Database(Settings.get_db_async_url())
 
 
-async def get_db():
+async def __get_db():
     async for session in db.get_sesion():
         yield session
 
-def get_jwt_service(db: AsyncSession = Depends(get_db)) -> JwtService:
+def get_jwt_service(db: AsyncSession = Depends(__get_db)) -> JwtService:
     return JwtService(Settings.get_jwt_secret(), RefreshTokenRepository(db))
 
 def get_current_user(token: str = Depends(oauth2_scheme),
                      jwt_service: JwtService = Depends(get_jwt_service)) -> dict:
     return jwt_service.decode(token)
+
+def get_auth_service(db: AsyncSession = Depends(__get_db)) -> AuthService:
+    return AuthService(UserRepository(db))
 
