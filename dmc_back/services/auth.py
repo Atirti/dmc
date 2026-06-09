c505e773226556ab7b5fda85cb484607b09195b5
Atirti <139679060+Atirti@users.noreply.github.com>
2026-06-09 18:17:00 +0500

    add login and registration in auth service

---
+++ dmc_back/services/auth.py
@@ -0,0 +1,25 @@
+from bcrypt import hashpw, checkpw, gensalt
+
+from models import User
+from repositories import UserRepository
+
+from fastapi import HTTPException, status
+
+class AuthService:
+    def __init__(self, repository: UserRepository):
+        self.__user_repository = repository
+
+    async def login(self, username, password) -> User:
+        user = await self.__user_repository.get_user_by_login(username)
+
+        if user is not None:
+            if checkpw(password.encode(), user.password.encode()):
+                return user
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
+
+    async def registration(self, username, password) -> User:
+        existing = await self.__user_repository.get_user_by_login(username)
+        if existing:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Username already taken")
+
+        return await self.__user_repository.create_user(username, hashpw(password.encode(), gensalt()).decode())
