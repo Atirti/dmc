56c4fe49985a1f0e01a1b986810ac464381fa4a4
Atirti <139679060+Atirti@users.noreply.github.com>
2026-06-09 18:07:23 +0500

    add func for find user by username, and create user

---
+++ dmc_back/repositories/user.py
@@ -0,0 +1,27 @@
+from sqlalchemy import insert, select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+from models import User
+
+
+class UserRepository:
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def get_user_by_login(self, username: str) -> User | None:
+        user = await self.db.execute(
+            select(User)
+            .where(User.username == username)
+        )
+
+        return user.scalar_one_or_none()
+
+    async def create_user(self, username: str, password: str) -> User:
+        result = await self.db.execute(
+            insert(User).values(username=username, password=password)
+            .returning(User)
+            )
+        await self.db.commit()
+
+        return result.scalar_one()
