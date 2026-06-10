0c1458865fee1a602db66608853b38fb5d68d5af
Atirti <139679060+Atirti@users.noreply.github.com>
2026-06-10 17:49:02 +0500

    add logout_everywhere

--- dmc_back/routers/auth.py
+++ dmc_back/routers/auth.py
@@ -5,6 +5,7 @@ from services import AuthService, JwtService
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
+
 @router.post("/login", response_model=LoginResponse)
 async def login(user_model: LoginRequest,
                 auth_service: AuthService = Depends(dependencies.get_auth_service),
@@ -17,10 +18,11 @@ async def login(user_model: LoginRequest,
 
     return dct
 
+
 @router.post("/registration", response_model=LoginResponse)
 async def registration(user_model: LoginRequest,
-                auth_service: AuthService = Depends(dependencies.get_auth_service),
-                jwt_service: JwtService = Depends(dependencies.get_jwt_service)) -> LoginResponse:
+                       auth_service: AuthService = Depends(dependencies.get_auth_service),
+                       jwt_service: JwtService = Depends(dependencies.get_jwt_service)) -> LoginResponse:
     """
     returns jwt_token and refresh_token
     """
@@ -32,7 +34,7 @@ async def registration(user_model: LoginRequest,
 
 @router.post("/refresh_token", response_model=TokenResponse)
 async def refresh_token(token_model: TokenRequest,
-                        jwt_service = Depends(dependencies.get_jwt_service)) -> TokenResponse:
+                        jwt_service=Depends(dependencies.get_jwt_service)) -> TokenResponse:
     """
     update jwt and refresh tokens
     returns jwt_token and refresh_token
@@ -41,10 +43,17 @@ async def refresh_token(token_model: TokenRequest,
 
     return dct
 
+
 @router.post("/logout")
 async def logout(request: LogoutRequest,
-                 jwt_service = Depends(dependencies.get_jwt_service)) -> None:
+                 current_user: dict = Depends(dependencies.get_current_user),
+                 jwt_service=Depends(dependencies.get_jwt_service)) -> None:
     """delete refresh token"""
-    await jwt_service.delete_refresh_token(request.refresh_token)
+    await jwt_service.delete_refresh_token(request.refresh_token, current_user["user_id"])
 
 
+@router.delete("/logout_everywhere")
+async def logout_everywhere(current_user: dict = Depends(dependencies.get_current_user),
+                            jwt_service=Depends(dependencies.get_jwt_service)) -> None:
+    """delete all user refresh tokens using id from jwt token"""
+    await jwt_service.delete_all_refresh_tokens(current_user["user_id"])
