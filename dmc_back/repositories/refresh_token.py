a12d27386c81ca25d2f3ec1700683b04877c0120
Atirti <139679060+Atirti@users.noreply.github.com>
2026-06-12 03:41:48 +0500

    add documentation

--- dmc_back/repositories/refresh_token.py
+++ dmc_back/repositories/refresh_token.py
@@ -7,12 +7,15 @@ from models import RefreshToken, User
 
 
 class RefreshTokenRepository:
+    """
+    Class for queries to refresh_tokens table.
+    """
     def __init__(self, db: AsyncSession):
         self.db = db
 
     async def add_refresh_token(self, user_id: int, refresh_token: str, refresh_exp: datetime) -> None:
         """
-        insert refresh token with expires date
+        Insert refresh token with expires date
         """
         await self.db.execute(
             insert(RefreshToken).values(user_id=user_id, token=refresh_token, expires_at=refresh_exp)
@@ -22,7 +25,7 @@ class RefreshTokenRepository:
     async def update_refresh_token(self, user_id: int, old_refresh_token: str, new_refresh_token: str,
                                    refresh_exp: datetime) -> None:
         """
-        replace old token with new, also update expires date
+        Replace old token with new, also update expires date
         """
         await self.db.execute(
             update(RefreshToken)
@@ -32,12 +35,18 @@ class RefreshTokenRepository:
         await self.db.commit()
 
     async def delete_refresh_token(self, refresh_token: str, user_id: int) -> None:
+        """
+        Delete one of user's refresh tokens
+        """
         await self.db.execute(
             delete(RefreshToken).where(RefreshToken.token == refresh_token, RefreshToken.user_id == user_id)
         )
         await self.db.commit()
 
     async def delete_all_refresh_tokens(self, user_id: int) -> None:
+        """
+        Delete all user's refresh tokens
+        """
         await self.db.execute(
             delete(RefreshToken).where(RefreshToken.user_id == user_id)
         )
@@ -45,7 +54,7 @@ class RefreshTokenRepository:
 
     async def get_user_by_refresh_token(self, refresh_token: str) -> User | None:
         """
-        find and return user by refresh token
+        Find and return user by refresh token
         """
         user = await self.db.execute(
             select(User)
