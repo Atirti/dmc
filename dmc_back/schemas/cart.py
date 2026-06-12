dc9790421ac51608573a44a5ae1bfcd2880d750c
Atirti <139679060+Atirti@users.noreply.github.com>
2026-06-13 03:40:21 +0500

    add cart endpoints and validators

---
+++ dmc_back/schemas/cart.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, field_validator
+from schemas.products import ProductModel as p
+
+
+class ProductModel(p):
+    count_in_cart: int
+
+
+class Request(BaseModel):
+    id: int
+    count: int
+
+    @field_validator('id')
+    @classmethod
+    def id_validator(cls, v):
+        if v < 0:
+            raise ValueError('id must be >= 0')
+        return v
+
+    @field_validator('count')
+    @classmethod
+    def count_validator(cls, v):
+        if v < 1:
+            raise ValueError('count must be >= 1')
+        return v
