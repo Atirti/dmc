000b46bcbda1c460c5b99b195ba33463e3d76fdf
Etrei1 <catinthenight89@gmail.com>
2026-06-15 00:59:58 +0500

    Перевод UI на MUI для стиля MD для левой панели, так же добавлена адаптивность

--- dmc_front/src/View/LeftPanel/LeftPanel.jsx
+++ dmc_front/src/View/LeftPanel/LeftPanel.jsx
@@ -1,28 +1,43 @@
-import { MdAccountCircle, MdHome, MdShoppingCart} from "react-icons/md";
-import { NavLink} from "react-router-dom";
+import {
+    MdOutlineAccountCircle,
+    MdOutlineHome,
+    MdOutlineShoppingCart,
+} from "react-icons/md";
+import { NavLink } from "react-router-dom";
+import {  IconButton } from "@mui/material";
 import "./LeftPanelCSS.css";
 
-
 function LeftPanel() {
-
     return (
             <div className="LeftPanel">
                 <header className="shopTitle">DMC Store</header>
 
                 <div className="menu">
-                    <NavLink to="/profile" className={({ isActive }) => isActive ? "active" : ""}>
-                        <MdAccountCircle size={25} />
-                        Профиль</NavLink>
+                    <NavLink to="/profile" className={({ isActive }) => isActive ? "navButton active" : "navButton"}>
+                        <IconButton className="menuButton" disableRipple>
+                            <div className="iconWrapper">
+                                <MdOutlineAccountCircle size={25} />
+                            </div>
+                            <span>Профиль</span>
+                        </IconButton>
+                    </NavLink>
 
-                    <NavLink to="/home" className={({ isActive }) => isActive ? "active" : ""}>
-                        <MdHome size={25} />
-                        Главная</NavLink>
+                    <NavLink to="/home" className={({ isActive }) => isActive ? "navButton active" : "navButton"}>
+                        <IconButton className="menuButton" disableRipple>
+                            <div className="iconWrapper">
+                                <MdOutlineHome size={25} />
+                            </div>
+                            <span>Главная</span>
+                        </IconButton>
+                    </NavLink>
 
-                    <NavLink
-                            to="/cart"
-                            className={({ isActive }) => isActive ? "cartLink active" : "cartLink"}>
-                        <MdShoppingCart size={25} />
-                        Корзина
+                    <NavLink to="/cart" className={({ isActive }) => isActive ? "navButton active" : "navButton"}>
+                        <IconButton className="menuButton" disableRipple>
+                            <div className="iconWrapper">
+                                <MdOutlineShoppingCart size={25} />
+                            </div>
+                            <span>Корзина</span>
+                        </IconButton>
                     </NavLink>
                 </div>
             </div>
