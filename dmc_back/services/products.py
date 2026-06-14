50b08fab9812263a1a537b4c6723014e033b6f88
Atirti <139679060+Atirti@users.noreply.github.com>
2026-06-15 03:45:13 +0500

    create add product and get product methods

--- dmc_back/services/products.py
+++ dmc_back/services/products.py
@@ -1,5 +1,7 @@
-from repositories import ProductRepository,  CategoryRepository
-from schemas.products import ProductsRequest
+from pydantic_settings.sources.providers import aws
+from fastapi import HTTPException, status
+from repositories import ProductRepository, CategoryRepository
+from schemas.products import ProductsRequest, ProductRequest, RequestId
 
 
 class ProductsService:
@@ -19,6 +21,17 @@ class ProductsService:
                                                                             sort=request.sort,
                                                                             order=request.order,
                                                                             category_id=request.category_id)
+
+    async def get_product(self, request: RequestId):
+        product = await self.__product_repository.get_product(id=request.id)
+        if product is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+        return product
+
+    async def add_product(self, request: ProductRequest):
+        return await self.__product_repository.add_product(request.title, request.description, request.price,
+                                                           request.picture_url, request.count_in_stock,
+                                                           request.category_id)
 
     async def get_categories(self):
         return await self.__category_repository.get_all_categories()
