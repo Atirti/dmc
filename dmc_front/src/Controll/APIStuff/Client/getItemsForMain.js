5847adc2c63aefcd48f02f4d40521d119de2a4ea
Etrei1 <catinthenight89@gmail.com>
2026-06-16 03:25:51 +0500

    changed item request added sort type category

--- dmc_front/src/Controll/APIStuff/Client/getItemsForMain.js
+++ dmc_front/src/Controll/APIStuff/Client/getItemsForMain.js
@@ -1,13 +1,16 @@
 import { api_url, parseError } from "../Autentification/auth.js";
 
-export async function getItemsRequest(_limit = 10, _offset = 0, _order = "desc", _sort = "date") {
+export async function getItemsRequest(_limit = 10, _offset = 0, _order = "desc", _sort = "date", _categoryId="") {
     const params = new URLSearchParams({
         limit: _limit,
         offset: _offset,
         sort: _sort,
         order: _order,
+        categotyId: _categoryId
     });
-
+    if (_categoryId !== "") {
+        params.append("category_id", _categoryId);
+    }
     const response = await fetch(`${api_url}/?${params.toString()}`, {
         method: "GET",
     });
