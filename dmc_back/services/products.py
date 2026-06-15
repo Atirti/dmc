6651502126aa0425c7006595d31106da41bc51e0
Atirti <139679060+Atirti@users.noreply.github.com>
2026-06-15 16:04:27 +0500

    add new methods in products service

--- dmc_back/services/products.py
+++ dmc_back/services/products.py
@@ -3,7 +3,7 @@ from fastapi import HTTPException, status
 from sqlalchemy.util import await_only
 
 from repositories import ProductRepository, CategoryRepository
-from schemas.products import ProductsRequest, ProductRequest, RequestId, CategoryModel
+from schemas.products import ProductsRequest, ProductRequest, RequestId, CategoryModel, ProductModel
 
 
 class ProductsService:
@@ -35,6 +35,20 @@ class ProductsService:
                                                            request.picture_url, request.count_in_stock,
                                                            request.category_id)
 
+    async def update_product(self, request: ProductModel):
+        result = await self.__product_repository.update_product(request.id, request.title, request.description,
+                                                                request.price,
+                                                                request.picture_url, request.count_in_stock,
+                                                                request.category_id)
+        if result is None:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Input values")
+        return result
+
+    async def delete_product(self, request: RequestId):
+        if await self.__product_repository.get_product(id=request.id) is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product not found")
+        await self.__product_repository.delete_product(request.id)
+
     async def get_categories(self):
         return await self.__category_repository.get_all_categories()
 
@@ -55,7 +69,7 @@ class ProductsService:
 
     async def remove_category(self, category_id: int):
         if any(await self.__product_repository.get_products_by_category(
-            limit=1, category_id=category_id, offset = 0, sort = "date", order = "desc"
+                limit=1, category_id=category_id, offset=0, sort="date", order="desc"
         )):
             raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Category in use")
         await self.__category_repository.delete_category(category_id)
